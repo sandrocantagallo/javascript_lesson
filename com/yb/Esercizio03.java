98f152c2bc39ea07fa78923458d2b8abc525dc81
sandrocantagallo <cantagallo@y-b.it>
2021-12-30 00:34:33 +0100

    piccole correzzioni ma il programma ancora non piace se scrivo un numero enorme mi va in errore. Se scrivo una stringa mi va in errore. Se scrivo cose strane va proprio in errore il programma come se le eccezzioni non vengano gestite

--- com/yb/Esercizio03.java
+++ com/yb/Esercizio03.java
@@ -8,22 +8,23 @@ import java.util.Scanner;
 
 public class Esercizio03 {
     public static void main(String[] args) {
-        Scanner scan  = new Scanner(System.in);
-        System.out.println("Digita il primo numero");
-        int a = scan.nextInt();
-        System.out.println("Digita il secondo numero");
-        int b = scan.nextInt();
-        System.out.println("Digita il terzo numero");
-        int c = scan.nextInt();
-        System.out.print("Il numero più grande è: ");
-        if ((a > b) && (a > c)) {
-            System.out.println(a);
-        }
-        if ((b > a) && (b > c)) {
-            System.out.println(b);
-        }
-        if ((c > a) && (c > b)) {
-            System.out.println(b);
+        try (Scanner scan = new Scanner(System.in)) {
+            System.out.println("Digita il primo numero");
+            long a = scan.nextInt();
+            System.out.println("Digita il secondo numero");
+            long b = scan.nextInt();
+            System.out.println("Digita il terzo numero");
+            long c = scan.nextInt();
+            System.out.print("Il numero più grande è: ");
+            if ((a > b) && (a > c)) {
+                System.out.println(a);
+            }
+            if ((b > a) && (b > c)) {
+                System.out.println(b);
+            }
+            if ((c > a) && (c > b)) {
+                System.out.println(b);
+            }
         }
     }
 }
